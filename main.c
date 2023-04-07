27a819e06c44fadaa2c86a878af4ffab097330b9
devSparta <31nikita31@gmail.com>
2023-04-07 19:07:06 +0300

    append push / pop functions

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
+#include <assert.h>
 #include "dataStructures/vector/vector.h"
 
+void test_pushBack_emptyVector() {
+    vector v = createVector(3);
+    pushBack(&v, 2);
+    pushBack(&v, 3);
+    pushBack(&v, 8);
+
+    assert(v.size == 3 && v.capacity == 3 && v.data[0] == 2 && v.data[1] == 3
+    && v.data[2] == 8);
+}
+
+void test_pushBack_fullVector() {
+    vector v = createVector(3);
+    pushBack(&v, 2);
+    pushBack(&v, 3);
+    pushBack(&v, 8);
+    pushBack(&v, 123);
+
+    assert(v.size == 4 && v.capacity == 6);
+}
+
+void test_popBack_notEmptyVector() {
+    vector v = createVector(0);
+    pushBack(&v, 10);
+
+    assert(v.size == 1);
+    popBack(&v);
+    assert(v.size == 0);
+    assert(v.capacity == 1);
+}
+
+void test() {
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+}
+
 int main() {
-    vector v = createVector(SIZE_MAX);
+    test();
 
     return 0;
 }
